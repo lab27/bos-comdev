cd239fe2cee34f8d3fccf7f14d1e3bbbb456afa2
Nick Niles <nickyniles@gmail.com>
2017-02-01 18:00:48 +0100

    add menu

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,14 +1,13 @@
 
 
 
-TweenMax.staggerFrom('.callout',.5,{opacity:0, marginTop: 100},.2);
+//toggle help
+$('#mobile-menu-toggler, #main-menu-toggler').on('click',function(){
+	console.log("toggle that menu!")
+	TweenMax.to($('#helpbar'),.2,{autoAlpha:1})
+});
 
-var tl = new TimelineLite()
 
-$('.callout').on('click', function(){
-	tl.to(this,.1,{borderTopWidth:10, borderBottomWidth:10, borderColor: "#900"})
-	.to(this,.2,{color: "rgba(0,0,0,0)", backgroundColor: "#f00"},"-=.2")
-	.to(this,.3,{opacity:0,height:0,ease: Back.easeIn, onComplete: function(){
-		(this.target).remove()
-	}});
+$('#close-help').on('click',function(){
+	TweenMax.to($('#helpbar'),.2,{autoAlpha:0})
 });
